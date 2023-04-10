9081735a387de213f320de72509d1a01915d1580
Gabriel Souza <gabriel.souza2004@outlook.com.br>
2023-04-10 16:25:34 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp15/Estacionamento.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Estacionamento
+    {
+        public int PrecoInicial;
+        public int PrecoHora;
+        private List<string> veiculos = new List<string>();
+
+        public void AdicionarVeiculos()
+        {
+            Console.WriteLine("Digite a placa do veículo para ser adicionado:");
+            string placa = Console.ReadLine().ToUpper();
+            veiculos.Add(placa);
+        }
+
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para ser removido:");
+            string removerPlaca = Console.ReadLine().ToUpper();
+
+            for (int i = 0; i < veiculos.Count; i++)
+            {
+                if (veiculos[i] == removerPlaca)
+                {
+                    veiculos.Remove(removerPlaca);
+                    Console.WriteLine(removerPlaca + " foi removido");
+                } else
+                {
+                    Console.WriteLine("Veículo não encontrado");
+                }
+            }
+
+        }
+
+        public void ListarVeiculos()
+        {
+            Console.WriteLine("Lista de veículos");
+            for (int i = 0; i < veiculos.Count; i++)
+            {
+                Console.WriteLine(veiculos[i]);
+            }
+        }
+    }
+}
---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estacionamento i = new Estacionamento();
+
+            Console.WriteLine("Bem vindo ao sistema de estacionamento!");
+            Console.WriteLine("Clique em alguma tecla para acessar nosso sistema.");
+            Console.ReadLine();
+
+            bool menu = true;
+
+            while (menu)
+            {
+                Console.Clear();
+                Console.WriteLine("1 - Cadastrar Veículo");
+                Console.WriteLine("2 - Remover Veículo");
+                Console.WriteLine("3 - Listar Veículo");
+                Console.WriteLine("4 - Encerrar");
+
+                switch (Console.ReadLine()) 
+                {
+                    case "1":
+                        i.AdicionarVeiculos();
+                        break;
+
+                    case "2":
+                        i.RemoverVeiculo();
+                        break;
+
+                    case "3":
+                        i.ListarVeiculos();
+                        break;
+
+                    case "4":
+                        menu = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção não encontrada");
+                        break;
+
+                }
+
+                Console.WriteLine("Pressione uma tecla para continuar");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
